30cc8f12d23f3ba66d40b2b1f53543282ef4c1c8
moresach <moresach@fdd27ae1-161a-0410-aa69-134b70899750>
2006-08-23 09:44:21 +0000

    Fixed the problem with test class, corrected imports.

--- trunk/src/com/omniscient/log4jcontrib/swingappender/ui/test/SwingAppenderTest.java
+++ trunk/src/com/omniscient/log4jcontrib/swingappender/ui/test/SwingAppenderTest.java
@@ -1,11 +1,11 @@
 package com.omniscient.log4jcontrib.swingappender.ui.test;
 
 import java.net.URL;
-import java.util.logging.Logger;
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-/**Test class for SwingAppenderUI.
+/**Test class for SwingAppende.
  * This is NOT a unit test class. It creates a SwingAppenderUI
  * and sends log statements to it. The target class has to be
  * tested manually.
@@ -15,22 +15,20 @@ public class SwingAppenderTest {
 
 	static{
 		URL configFileUrl = ClassLoader.getSystemResource("log4j.properties");
-		System.out.println("Config File - "+configFileUrl);
-		PropertyConfigurator.configure(configFileUrl.getPath());
+		System.out.println("Config File - " + configFileUrl.getPath());
+		PropertyConfigurator.configure(configFileUrl);
 	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger("name");
-		//SwingAppenderUI loggerWin = new SwingAppenderUI();
-		//test
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 1000; i++) {
 			try {
-				Thread.sleep(100);
+				Thread.sleep(500);
 			} catch (InterruptedException ie) {
 			}
-			// loggerWin.doLog("message " + i);
 			logger.info("message  " + i);
 		}
 	}
